48d0cb53f145135148225487877812cb3553e9db
Leandro486 <duarte.nazz@gmail.com>
2023-09-30 22:39:06 +0100

    Update ExtrairDados.py

--- teste-slim-v4/src/Application/Actions/ExtractionAPIs/ExtrairDados.py
+++ teste-slim-v4/src/Application/Actions/ExtractionAPIs/ExtrairDados.py
@@ -12,11 +12,19 @@ subreddit = reddit.subreddit('TesteLP')
 
 keyword = 'Exemplo'
 
-for submission in subreddit.hot(limit=10):  
-    submission.comments.replace_more(limit=None)  
+for submission in subreddit.hot(limit=10):  # Limite de 10 postagens (você pode ajustar isso)
+    if keyword in submission.title:
+        print(f'Título: {submission.title}')
+        print(f'Autor: {submission.author}')
+        print(f'Pontuação: {submission.score}')
+        print(f'URL: {submission.url}')
+    
+    # Itere pelos comentários de cada postagem
+    submission.comments.replace_more(limit=None)  # Carregar todos os comentários
     for comment in submission.comments.list():
-        if keyword in comment.body.lower():  
-            print(f'Comentário encontrado: {comment.body}')
-
+        print(f'Comentário: {comment.body}')
+        print(f'Autor do Comentário: {comment.author}')
+        print(f'Pontuação do Comentário: {comment.score}')
+        print('---')
 
 
