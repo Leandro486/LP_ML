a1a8840f2b4376db4296bebe67c48006d3626790
Leandro486 <duarte.nazz@gmail.com>
2023-10-10 15:40:39 +0100

    Update ExtrairDados.py

--- teste-slim-v4/src/Application/Actions/ExtractionAPIs/ExtrairDados.py
+++ teste-slim-v4/src/Application/Actions/ExtractionAPIs/ExtrairDados.py
@@ -1,12 +1,12 @@
-import praw
+import praw #reddit
+import tweepy #twitter
 
-
+#API REDDIT
 reddit = praw.Reddit(
     client_id='XWLkBoloGyiakQmNzfGPSg',
     client_secret='nXCSJXIZhR23eJCdYJbEM2t6F2nTDA',
     user_agent='Leandro&Filhos'
 )
-
 
 subreddit = reddit.subreddit('TesteLP')
 
@@ -27,3 +27,18 @@ for submission in subreddit.hot(limit=10):  # Limite de 10 postagens (você pode
         print(f'Pontuação do Comentário: {comment.score}')
         print('---')
 
+#API FACEBOOK
+
+
+
+#API TWITTER
+
+twitter = [
+    client_key='gOR0lfXzYDgmi3HE92X1MFxZC', 
+    client_secret='BQrR5ekfA8ji1O1IQB7GVpxCiX3UTJlymVaoHafPSNjAGfuTxX',
+    access_token='1706701378712633344-iyNfzFKAcpg3FeGXJ9swbkyziXNtxG',
+    access_token_secret='c8aaSir9XjjS5QbmEAvbiGgPj67dg2J696ZHTq9GBGylr'
+]
+
+
+auth = tweepy.OAuthHandler(twitter.client_key,twitter.client_secret)
